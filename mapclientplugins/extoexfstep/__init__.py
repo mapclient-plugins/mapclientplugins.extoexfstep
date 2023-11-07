e780952fb8c8a6151343196d0468274ac99ad442
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-11-08 11:17:24 +1300

    Set location metadata.

--- mapclientplugins/extoexfstep/__init__.py
+++ mapclientplugins/extoexfstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin - Generated from MAP Client v0.17.3
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Ex to Exf'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.extoexfstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.extoexfstep import step
